2b5b763ee27c95dbc2c3cc86c062f6b7f4fa50a9
Geospecial <bizcuit@gmail.com>
2016-09-12 12:13:10 -0400

    change routepath duration

--- webmaps/js/rm10k.js
+++ webmaps/js/rm10k.js
@@ -279,7 +279,7 @@
 
         function transition(path) {
             path.transition()
-                .duration(9500)
+                .duration(1200)
                 .attrTween("stroke-dasharray", tweenDash)
                 //if you want to have it repeat the sequence
                  //then uncomment this piece
